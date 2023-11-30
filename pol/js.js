01e7472de01e443ba42d5cadaf56bb51f4aefab8
almamm43 <152469153+almamm43@users.noreply.github.com>
2023-11-29 23:30:36 -0600

    Add files via upload

---
+++ pol/js.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAHbsRElcFYBa-r9_-gZXLQYJQqy3GBXjs",
+    authDomain: "aplicacion-9a033.firebaseapp.com",
+    projectId: "aplicacion-9a033",
+    storageBucket: "aplicacion-9a033.appspot.com",
+    messagingSenderId: "198267131464",
+    appId: "1:198267131464:web:c45e6fde3bea7140a16a7d",
+    measurementId: "G-9PV2B05D91"
+    });
